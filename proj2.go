5a55483fb50ffeaa3a6d9dba246c69b62750521a
Tan Jun Ming <junming1996@hotmail.com>
2019-11-01 23:58:48 -0700

    share and receive files

--- proj2.go
+++ proj2.go
@@ -82,12 +82,30 @@ func bytesToUUID(data []byte) (ret uuid.UUID) {
 type User struct {
 	Username string
 	PasswordHash []byte
+	Salt []byte
+	SymmetricKey []byte
+	HmacKey []byte
 	PrivateKey userlib.PKEDecKey
 	SignKey userlib.DSSignKey
+	Uuid uuid.UUID
+	Files map[string]File
 
 	// You can add other fields here if you want...
 	// Note for JSON to marshal/unmarshal, the fields need to
 	// be public (start with a capital letter)
+}
+
+type File struct {
+	Uuid uuid.UUID
+	SymmeticKey []byte
+	HmacKey []byte
+}
+
+type SharingRecord struct {
+	HmacKey []byte //to ensure integrity
+	FileUuid uuid.UUID
+	FileSymmetricKey []byte
+	FileHmacKey []byte
 }
 
 // This creates a user.  It will only be called once for a user
@@ -109,13 +127,14 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 	var userdata User
 	userdataptr = &userdata
 
+	userdata.Files = make(map[string]File)
 	userdata.Username = username
-	salt := userlib.RandomBytes(16)
+	userdata.Salt = userlib.RandomBytes(16)
 
 	//generate 32 byte key from password
-	keyString := userlib.Argon2Key([]byte(password), salt, 32)
-	symmetricKey := keyString[:16]
-	hmacKey := keyString[16:]
+	keyString := userlib.Argon2Key([]byte(password), userdata.Salt, 32)
+	userdata.SymmetricKey = keyString[:16]
+	userdata.HmacKey = keyString[16:]
 
 	//generate user's key pairs
 	publicKey,privateKey,_ := userlib.PKEKeyGen()
@@ -129,19 +148,21 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 	userlib.KeystoreSet(username+"pke", publicKey)
 	userlib.KeystoreSet(username+"ds", verifyKey)
 
+	//generate and store user uuid
+	uuidString := userlib.Argon2Key([]byte(username+password), []byte(""), 16)
+	userdata.Uuid,_ = uuid.FromBytes(uuidString)
+
 	//encrypt userdata
 	jsonUserdata,_ := json.Marshal(userdata)
-	encrypted := userlib.SymEnc(symmetricKey, userlib.RandomBytes(16), jsonUserdata)
-	ciphertext := append(salt, encrypted...)
+	encrypted := userlib.SymEnc(userdata.SymmetricKey, userlib.RandomBytes(16), jsonUserdata)
+	ciphertext := append(userdata.Salt, encrypted...)
 
 	//hmac encrypted data
-	hmac,_ := userlib.HMACEval(hmacKey, encrypted)
+	hmac,_ := userlib.HMACEval(userdata.HmacKey, encrypted)
 	securedData := append(hmac, ciphertext...)
 
 	//store secured data to datastore
-	//userUuid := bytesToUUID([]byte(username+password))
-	userUuid,_ := uuid.FromBytes([]byte(username+password))
-	userlib.DatastoreSet(userUuid, securedData)
+	userlib.DatastoreSet(userdata.Uuid, securedData)
 
 	return userdataptr, nil
 }
@@ -154,14 +175,15 @@ func GetUser(username string, password string) (userdataptr *User, err error) {
 	userdataptr = &userdata
 
 	//retrieve secured data from datastore
-	userUuid,_ := uuid.FromBytes([]byte(username+password))
+	uuidString := userlib.Argon2Key([]byte(username+password), []byte(""), 16)
+	userUuid,_ := uuid.FromBytes(uuidString)
 	securedData,fileExist := userlib.DatastoreGet(userUuid)
 	if !fileExist {
 		return userdataptr, errors.New("invalid username/password")
 	}
-	hmac := securedData[:16]
-	salt := securedData[16:32]
-	ciphertext := securedData[32:]
+	hmac := securedData[:64]
+	salt := securedData[64:80]
+	ciphertext := securedData[80:]
 
 	//generate 32 byte key from password
 	keyString := userlib.Argon2Key([]byte(password), salt, 32)
@@ -170,13 +192,16 @@ func GetUser(username string, password string) (userdataptr *User, err error) {
 
 	//check if file is corrupted or tampered
 	receivedHmac,_ := userlib.HMACEval(hmacKey, ciphertext)
-	if userlib.HMACEqual(hmac, receivedHmac) {
+	if !userlib.HMACEqual(hmac, receivedHmac) {
 		return nil, errors.New("userdata corrupted")
 	}
 
 	//decrypt encrypted user data
 	decrypted := userlib.SymDec(symmetricKey, ciphertext)
-	json.Unmarshal(decrypted, userdataptr)
+	err = json.Unmarshal(decrypted, userdataptr)
+	if err != nil{
+		return nil, errors.New("decrypt failed")
+	}
 
 	return userdataptr, nil
 }
@@ -185,8 +210,42 @@ func GetUser(username string, password string) (userdataptr *User, err error) {
 //
 // The name and length of the file should NOT be revealed to the datastore!
 func (userdata *User) StoreFile(filename string, data []byte) {
-	fileUuid := bytesToUUID(userlib.H)
 
+	//create file uuid
+	fileUuidString := userlib.Argon2Key([]byte(userdata.Username+filename), []byte(""), 16)
+	fileUuid,_ := uuid.FromBytes(fileUuidString)
+
+	//create new key to encrypt file
+	keyString,_ := userlib.HMACEval(userlib.RandomBytes(16), data)
+	symmetricKey := keyString[:16]
+	hmacKey := keyString[16:32]
+
+	//encrypt file data with symmetric key
+	encryptedFile := userlib.SymEnc(symmetricKey, userlib.RandomBytes(16), data)
+	hmacFile,_ := userlib.HMACEval(hmacKey, encryptedFile)
+	secured := append(hmacFile, encryptedFile...)
+
+	//store file to datastore
+	userlib.DatastoreSet(fileUuid, secured)
+
+	//write file mapping to userdata
+	var file File
+	file.Uuid = fileUuid
+	file.SymmeticKey = symmetricKey
+	file.HmacKey = hmacKey
+	userdata.Files[filename] = file
+
+	//encrypt userdata
+	jsonUserdata,_ := json.Marshal(userdata)
+	encrypted := userlib.SymEnc(userdata.SymmetricKey, userlib.RandomBytes(16), jsonUserdata)
+	ciphertext := append(userdata.Salt, encrypted...)
+
+	//hmac encrypted data
+	hmac,_ := userlib.HMACEval(userdata.HmacKey, encrypted)
+	securedData := append(hmac, ciphertext...)
+
+	//store secured data to datastore
+	userlib.DatastoreSet(userdata.Uuid, securedData)
 	return
 }
 
@@ -204,7 +263,25 @@ func (userdata *User) AppendFile(filename string, data []byte) (err error) {
 //
 // It should give an error if the file is corrupted in any way.
 func (userdata *User) LoadFile(filename string) (data []byte, err error) {
-	return
+
+	//retrieve file information from filename
+	file := userdata.Files[filename]
+	fileUuid := file.Uuid
+
+	//retrieve secured file from datastore
+	securedFile,_ := userlib.DatastoreGet(fileUuid)
+	hmac := securedFile[:64]
+	encryptedData := securedFile[64:]
+
+	//check if file is corrupted
+	receivedHmac,_ := userlib.HMACEval(file.HmacKey, encryptedData)
+	if !userlib.HMACEqual(hmac, receivedHmac) {
+		return nil, errors.New("file corrupted")
+	}
+
+	//decrypt encrypted file
+	data = userlib.SymDec(file.SymmeticKey, encryptedData)
+	return data, nil
 }
 
 // This creates a sharing record, which is a key pointing to something
@@ -221,7 +298,51 @@ func (userdata *User) LoadFile(filename string) (data []byte, err error) {
 func (userdata *User) ShareFile(filename string, recipient string) (
 	magic_string string, err error) {
 
-	return
+	//retrieve file information
+	file := userdata.Files[filename]
+
+	//generate uuid for sharing record
+	srUuidSeed := userlib.Argon2Key([]byte(userdata.Username+recipient+filename),
+		userlib.RandomBytes(16), 32)
+	srUuid,_ := uuid.FromBytes(srUuidSeed[:16])
+	fsrKey := srUuidSeed[16:]
+
+	//create a sharing record
+	var sharingRecord SharingRecord
+	sharingRecord.HmacKey = userlib.RandomBytes(16)
+	sharingRecord.FileUuid = file.Uuid
+	sharingRecord.FileSymmetricKey = file.SymmeticKey
+	sharingRecord.FileHmacKey = file.HmacKey
+
+
+	//encrypt sharing record with a random key
+	srJson,_ := json.Marshal(sharingRecord)
+	encryptedFSR := userlib.SymEnc(fsrKey, userlib.RandomBytes(16), srJson)
+
+	//add mac to encrypted sharing record
+	srHmac,_ := userlib.HMACEval(sharingRecord.HmacKey, encryptedFSR)
+	securedSr := append(srHmac, encryptedFSR...)
+
+	//store sharing record to data store
+	userlib.DatastoreSet(srUuid, securedSr)
+
+	//encrypt fsr key and store to datastore
+	recipientPubKey,_ := userlib.KeystoreGet(recipient+"pke")
+	encryptedFsrKey,err1 := userlib.PKEEnc(recipientPubKey, fsrKey)
+	if err1 != nil {
+		return "", err1
+	}
+	encryptedFsrKeyUuid := uuid.New()
+	userlib.DatastoreSet(encryptedFsrKeyUuid, encryptedFsrKey)
+
+	//generate magic string (sr uiid) and sign with ds
+	encryptedFsrKeyUuidByte,_ := encryptedFsrKeyUuid.MarshalBinary()
+	srUuidByte,_ := srUuid.MarshalBinary()
+	combined := append(encryptedFsrKeyUuidByte, srUuidByte...)
+	srDigSig,_ := userlib.DSSign(userdata.SignKey, combined)
+	magicByte := append(srDigSig, combined...)
+
+	return hex.EncodeToString(magicByte), nil
 }
 
 // Note recipient's filename can be different from the sender's filename.
@@ -230,10 +351,66 @@ func (userdata *User) ShareFile(filename string, recipient string) (
 // it is authentically from the sender.
 func (userdata *User) ReceiveFile(filename string, sender string,
 	magic_string string) error {
+
+
+	// verify digital signature
+	senderVerify,_ := userlib.KeystoreGet(sender+"ds")
+	magicByte,_ := hex.DecodeString(magic_string)
+	srDigSig := magicByte[:256]
+	combined := magicByte[256:]
+	signatureError := userlib.DSVerify(senderVerify, combined, srDigSig)
+	if signatureError != nil{
+		return signatureError
+	}
+
+	//extract information from combined
+	encryptedFsrKeyUuidByte := combined[:16]
+	encryptedFsrKeyUuid := bytesToUUID(encryptedFsrKeyUuidByte)
+	srUuidByte := combined[16:]
+	srUuid := bytesToUUID(srUuidByte)
+
+	//retrieve encrypted fsr key
+	encryptedFsrKey,_ := userlib.DatastoreGet(encryptedFsrKeyUuid)
+
+	//decrypt encrypted fsr key
+	fsrKey,_ := userlib.PKEDec(userdata.PrivateKey, encryptedFsrKey)
+
+	//retrieve secured fsr
+	securedFsr,_ := userlib.DatastoreGet(srUuid)
+	var sharingRecord SharingRecord
+	decryptedSR := userlib.SymDec(fsrKey, securedFsr[64:])
+	json.Unmarshal(decryptedSR, &sharingRecord)
+
+	//check sr hmac
+	receivedHmac,_ := userlib.HMACEval(sharingRecord.HmacKey, securedFsr[64:])
+	if !userlib.HMACEqual(securedFsr[:64], receivedHmac){
+		return errors.New("hmac not equal")
+	}
+
+	//write file to userdata
+	var file File
+	file.HmacKey = sharingRecord.FileHmacKey
+	file.Uuid = sharingRecord.FileUuid
+	file.SymmeticKey = sharingRecord.FileSymmetricKey
+	userdata.Files[filename] = file
+
+	//encrypt userdata
+	jsonUserdata,_ := json.Marshal(userdata)
+	encrypted := userlib.SymEnc(userdata.SymmetricKey, userlib.RandomBytes(16), jsonUserdata)
+	ciphertext := append(userdata.Salt, encrypted...)
+
+	//hmac encrypted data
+	hmac,_ := userlib.HMACEval(userdata.HmacKey, encrypted)
+	securedData := append(hmac, ciphertext...)
+
+	//store secured data to datastore
+	userlib.DatastoreSet(userdata.Uuid, securedData)
+
 	return nil
 }
 
 // Removes target user's access.
 func (userdata *User) RevokeFile(filename string, target_username string) (err error) {
+
 	return
 }
